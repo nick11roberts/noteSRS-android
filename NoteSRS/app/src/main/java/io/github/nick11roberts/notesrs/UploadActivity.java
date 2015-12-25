0314c9cd31af9ff7fac4ef2b4f806b2fbf5d31d4
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2015-12-24 23:30:10 -0800

    New correction navigation works properly.

--- NoteSRS/app/src/main/java/io/github/nick11roberts/notesrs/UploadActivity.java
+++ NoteSRS/app/src/main/java/io/github/nick11roberts/notesrs/UploadActivity.java
@@ -2,6 +2,7 @@ package io.github.nick11roberts.notesrs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class UploadActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class UploadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
