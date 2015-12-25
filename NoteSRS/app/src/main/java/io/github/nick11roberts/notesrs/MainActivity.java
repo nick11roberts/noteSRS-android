d164730b99b22f30ad5bc1911eacc1bf1793b59b
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2015-12-24 23:15:58 -0800

    Settings navigation is properly functioning.

--- NoteSRS/app/src/main/java/io/github/nick11roberts/notesrs/MainActivity.java
+++ NoteSRS/app/src/main/java/io/github/nick11roberts/notesrs/MainActivity.java
@@ -1,10 +1,12 @@
 package io.github.nick11roberts.notesrs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Intent myIntent = new Intent(MainActivity.this, UploadActivity.class);
+                MainActivity.this.startActivity(myIntent);
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -44,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
+            // Start the SettingsActivity
+            Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+            MainActivity.this.startActivity(myIntent);
+
             return true;
         }
 
